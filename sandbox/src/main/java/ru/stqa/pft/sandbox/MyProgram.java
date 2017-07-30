31f40c5a50d77c0d6968e6e5bf5151d19bbd6c83
lucjasniegota <lucjasniegota@gmail.com>
2017-07-30 18:41:50 +0200

    MyProgram -nowa klasa Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class MyProgram {
+    public static void main (String[] args){
+        Point p = new Point(1, 6);
+        System.out.println("dystans miedzy " + p.p1 + " a " + p.p2 + " wynosi " + p.distance() );
+
+    }
+
+
+}
