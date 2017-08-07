ec6e3fd98425fa4d200fc3e33882c9683d50369c
lucjasniegota <lucjasniegota@gmail.com>
2017-08-07 11:25:53 +0200

    stworzenie klasy Applicationmanager
    dwie warstwy projektu

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class ContactCreationTests extends TestBase {
@@ -9,9 +7,9 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    gotoNewContact();
-    fillContactForm(new ContactData("Łucja", "Śniegota", "lucjasniegota@gmail.com"));
-    returnHomePage();
+    app.gotoNewContact();
+    app.fillContactForm(new ContactData("Łucja", "Śniegota", "lucjasniegota@gmail.com"));
+    app.returnHomePage();
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -8,9 +7,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
 
-    gotoGroupPage();
-    selectGroup();
-    deleteGroup();
-    returnGroupPage();
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteGroup();
+    app.returnGroupPage();
   }
 }
