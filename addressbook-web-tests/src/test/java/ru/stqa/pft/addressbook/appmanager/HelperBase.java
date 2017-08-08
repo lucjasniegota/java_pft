0a6b7d183671408ac03311b51c0a3911bc8ac587
lucjasniegota <lucjasniegota@gmail.com>
2017-08-08 11:43:30 +0200

    stworzenie HelperBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelperBase {
+
+  protected FirefoxDriver wd;
+
+  public HelperBase(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  public void type(By locator, String text) {
+    click(locator);
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
+  }
+}
