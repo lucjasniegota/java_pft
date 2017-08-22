c58520150025c34983a0435be12bd6f015ab20db
lucjasniegota <lucjasniegota@gmail.com>
2017-08-22 18:03:09 +0200

    stworzenie listy grup dla before i after

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,12 +3,18 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactCreationTests extends TestBase {
 
 
   @Test
   public void testContactCreation() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", "test1", "trst1"));
+    }
+    app.getNavigationHelper().gotoHomePage();
     int before = app.getContactHelper().getContactCount();
     app.getContactHelper().gotoNewContact();
     app.getContactHelper().fillContactForm(
