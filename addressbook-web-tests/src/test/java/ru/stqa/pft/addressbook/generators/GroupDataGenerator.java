3b0afa0682536cd82acbe54378ea7e5dc62a3b88
lucjasniegota <lucjasniegota@gmail.com>
2017-09-02 18:54:34 +0200

    6.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,32 +13,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
-  public static void main (String [] args) throws IOException {
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
 
 
-int count = Integer.parseInt(args[0]);
-File file = new File(args[1]);
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jcommander = new JCommander(generator);
+    try {
+      jcommander.parse(args);
+    } catch (ParameterException ex) {
+      jcommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, file);
-}
+    save(groups, new File(file));
+  }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
-    for(GroupData group : groups){
-      writer.write(String.format("%s;%s;%s\n" , group.getName(), group.getHeader(), group.getFooter()));
-
+    for (GroupData group : groups) {
+      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
     }
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private  List<GroupData> generateGroups(int count)  {
     List<GroupData> groups = new ArrayList<GroupData>();
-    for (int i = 0; i < count; i++ ){
+    for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withName(String.format("test %s", i))
               .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
-
-    }    return groups;
+    }
+    return groups;
   }
 }
