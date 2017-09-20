fc98be2c78c12c2d59dbf2dea6eea13f1d7d2391
lucjasniegota <lucjasniegota@gmail.com>
2017-09-20 16:10:39 +0200

    8.4 leniwa inicjalizacja - registrationtest

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import ru.stqa.pft.mantis.tests.RegistrationTests;
 
 import java.io.File;
 import java.io.FileReader;
@@ -16,7 +17,8 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
   private final String browser;
   public final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
+  private RegistrationHelper registrationHelper;
 
 
   public ApplicationManager(String browser) {
@@ -28,20 +30,13 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));}
+}
 
 
   public void stop() {
+    if(wd !=null){
     wd.quit();
-  }
+  }}
 
 
   public String getProperty(String key) {
@@ -50,4 +45,24 @@ public class ApplicationManager {
   public HttpSession newSession (){
     return new HttpSession (this);
   }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper ==null) {
+      registrationHelper = new RegistrationHelper(this);
+    } return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+  if(wd ==null){
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+    return wd;}
 }
