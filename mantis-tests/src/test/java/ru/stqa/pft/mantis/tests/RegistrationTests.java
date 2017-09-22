adb36f8dffa3e2fb612480ae746ad9a780b0ab87
lucjasniegota <lucjasniegota@gmail.com>
2017-09-22 14:01:09 +0200

    coś nie działawysyłanie listu

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -4,25 +4,25 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
-import ru.stqa.pft.mantis.appmanager.MailMessage;
+import ru.stqa.pft.mantis.model.MailMessage;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
 public class RegistrationTests extends TestBase {
-// @BeforeMethod
-//public void startMailServer(){
-//  app.mail().start();
-//}
+@BeforeMethod
+public void startMailServer(){
+ app.mail().start();
+}
 
   @Test
   public void testRegistration() throws IOException, MessagingException {
- String email = "user1@localhost.localdomain";
-   app.registration().start("user1", email);
- //  List<MailMessage> mailMessages =  app.mail().waitForMail(2,10000);
- // String confirmationLink =  findConfirmationLink(mailMessages, email);
-//app.registration().finish(confirmationLink, "password");
+ String email = "user2@localhost.localdomain";
+   app.registration().start("user2", email);
+   List<MailMessage> mailMessages =  app.mail().waitForMail(2,10000);
+ String confirmationLink =  findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, "password");
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
@@ -32,8 +32,8 @@ public class RegistrationTests extends TestBase {
   }
 
 
- /* @AfterMethod(alwaysRun = true)
+@AfterMethod(alwaysRun = true)
   public void stopMailServer(){
     app.mail().stop();
-  }   */
+  }
 }
