40646896d743c7c50249df0662068092ef275966
lucjasniegota <lucjasniegota@gmail.com>
2017-09-29 12:49:52 +0200

    9.6 Rest z githubem

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+@Test
+  public void testCommits() throws IOException {
+  Github github = new RtGithub("a6e8dcfbc3aa3d153d13891cb2184e81452c4bbc");
+  RepoCommits commits = github.repos().get(new Coordinates.Simple("lucjasniegota", "java_pft")).commits();
+  for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+    System.out.println(commit);
+    System.out.println(new RepoCommit.Smart(commit).message());
+
+  }
+}
+}
--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -4,10 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
-import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -15,7 +16,11 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestAssuredTests {
+  @BeforeClass
+  public void init(){
+    RestAssured.authentication = RestAssured.basic("28accbe43ea112d9feb328d2c00b3eed", "");
+  }
   @Test
   public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues = getIssue();
@@ -27,21 +32,14 @@ public class RestTests {
   }
 
   private int createIssue(Issue newIssue) throws IOException {
-    String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
-            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                    new BasicNameValuePair("description", newIssue.getDescription())))
-            .returnContent().asString();
+    String json = RestAssured.given().parameter("subject", newIssue.getSubject())
+            .parameter("description", newIssue.getDescription()).post("http://demo.bugify.com/api/issues.json").asString();
   JsonElement parsed = new JsonParser().parse(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
 }
 
-  private Executor getExecutor(){
-    return Executor.newInstance().auth("28accbe43ea112d9feb328d2c00b3eed", "");
-  }
-
   private Set<Issue> getIssue() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-            .returnContent().asString();
+    String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
